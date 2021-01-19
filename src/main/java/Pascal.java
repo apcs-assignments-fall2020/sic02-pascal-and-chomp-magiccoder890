a6384a72d487c65cbb7d39fa57b6598e88e0e269
magiccoder890 <23fzhu@athenian.org>
2021-01-19 21:00:22 +0000

    it works

--- src/main/java/Pascal.java
+++ src/main/java/Pascal.java
@@ -8,6 +8,17 @@ public class Pascal
 {
     public static int[][] pascalTriangle(int n) {
         // YOUR CODE HERE
-        return null;
+        int [][] output = new int[n][n];
+        output[0][0] = 1;
+        for (int i = 1; i<n;i++){
+            output[i][0] = 1;
+            output[i][i] = 1;
+        }
+        for (int cul =2;cul < n;cul++){
+            for (int row = 1;row < cul;row ++){
+                output[cul][row] = output[cul-1][row-1] + output[cul-1][row];
+            }
+        }
+        return output;
     }
 }
